563a3ea5238a71ef15445566fea3b379a6adc38c
WorkuUrga <urgaworku@gmail.com>
2024-07-11 15:03:05 +0300

    corrections

--- quiz.js
+++ quiz.js
@@ -1,17 +1,16 @@
 
 
-const submitBtn = document.getElementById("submit-answer");
-submitBtn.addEventListener('click', checkAnswer);
-
 function checkAnswer() {
     const correctAnswer = 4;
     let userAnswer = parseInt(document.querySelector('input[name="quiz"]:checked').value, 10);
     let feedBack = document.querySelector('#feedback');
 
-    if(correctAnswer === userAnswer) {
+    if(userAnswer === correctAnswer) {
         feedBack.textContent = "Correct! Well done.";
     }
     else {
         feedBack.textContent = "That's incorrect. Try again!";
     };
     };
+    const submitBtn = document.getElementById("submit-answer");
+submitBtn.addEventListener('click', checkAnswer);
